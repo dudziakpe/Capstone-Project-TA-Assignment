70c86ca4d8aab1bac4ddf2a8e4d1465a53a102ff
Trey88-4 <103338623+Trey88-4@users.noreply.github.com>
2023-03-29 16:51:17 -0700

    Initial commit

---
+++ CS-TA-Assignments-main/CS TA Assignments/MainForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace cwu.cs.TaAssignments
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.txtApplications = new System.Windows.Forms.TextBox();
+            this.lblApplications = new System.Windows.Forms.Label();
+            this.btnApplications = new System.Windows.Forms.Button();
+            this.btnSchedule = new System.Windows.Forms.Button();
+            this.lblSchedule = new System.Windows.Forms.Label();
+            this.txtSchedule = new System.Windows.Forms.TextBox();
+            this.btnGrades = new System.Windows.Forms.Button();
+            this.lblGrades = new System.Windows.Forms.Label();
+            this.txtGrades = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.dlgOpenCsv = new System.Windows.Forms.OpenFileDialog();
+            this.gbxInput = new System.Windows.Forms.GroupBox();
+            this.gbxOutput = new System.Windows.Forms.GroupBox();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.txtResults = new System.Windows.Forms.TextBox();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.dlgSaveCsv = new System.Windows.Forms.SaveFileDialog();
+            this.gbxInput.SuspendLayout();
+            this.gbxOutput.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(11, 210);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(748, 430);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.WordWrap = false;
+            // 
+            // txtApplications
+            // 
+            this.txtApplications.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtApplications.Location = new System.Drawing.Point(73, 20);
+            this.txtApplications.Margin = new System.Windows.Forms.Padding(2);
+            this.txtApplications.Name = "txtApplications";
+            this.txtApplications.Size = new System.Drawing.Size(586, 20);
+            this.txtApplications.TabIndex = 2;
+            // 
+            // lblApplications
+            // 
+            this.lblApplications.AutoSize = true;
+            this.lblApplications.Location = new System.Drawing.Point(5, 23);
+            this.lblApplications.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblApplications.Name = "lblApplications";
+            this.lblApplications.Size = new System.Drawing.Size(64, 13);
+            this.lblApplications.TabIndex = 3;
+            this.lblApplications.Text = "Applications";
+            this.lblApplications.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnApplications
+            // 
+            this.btnApplications.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnApplications.Location = new System.Drawing.Point(663, 18);
+            this.btnApplications.Margin = new System.Windows.Forms.Padding(2);
+            this.btnApplications.Name = "btnApplications";
+            this.btnApplications.Size = new System.Drawing.Size(75, 23);
+            this.btnApplications.TabIndex = 4;
+            this.btnApplications.Text = "open";
+            this.btnApplications.UseVisualStyleBackColor = true;
+            this.btnApplications.Click += new System.EventHandler(this.btnApplications_Click);
+            // 
+            // btnSchedule
+            // 
+            this.btnSchedule.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSchedule.Location = new System.Drawing.Point(663, 45);
+            this.btnSchedule.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSchedule.Name = "btnSchedule";
+            this.btnSchedule.Size = new System.Drawing.Size(75, 23);
+            this.btnSchedule.TabIndex = 7;
+            this.btnSchedule.Text = "open";
+            this.btnSchedule.UseVisualStyleBackColor = true;
+            this.btnSchedule.Click += new System.EventHandler(this.btnSchedule_Click);
+            // 
+            // lblSchedule
+            // 
+            this.lblSchedule.AutoSize = true;
+            this.lblSchedule.Location = new System.Drawing.Point(17, 50);
+            this.lblSchedule.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSchedule.Name = "lblSchedule";
+            this.lblSchedule.Size = new System.Drawing.Size(52, 13);
+            this.lblSchedule.TabIndex = 6;
+            this.lblSchedule.Text = "Schedule";
+            this.lblSchedule.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtSchedule
+            // 
+            this.txtSchedule.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSchedule.Location = new System.Drawing.Point(73, 47);
+            this.txtSchedule.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSchedule.Name = "txtSchedule";
+            this.txtSchedule.Size = new System.Drawing.Size(586, 20);
+            this.txtSchedule.TabIndex = 5;
+            // 
+            // btnGrades
+            // 
+            this.btnGrades.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGrades.Location = new System.Drawing.Point(663, 72);
+            this.btnGrades.Margin = new System.Windows.Forms.Padding(2);
+            this.btnGrades.Name = "btnGrades";
+            this.btnGrades.Size = new System.Drawing.Size(75, 23);
+            this.btnGrades.TabIndex = 10;
+            this.btnGrades.Text = "open";
+            this.btnGrades.UseVisualStyleBackColor = true;
+            this.btnGrades.Click += new System.EventHandler(this.btnGrades_Click);
+            // 
+            // lblGrades
+            // 
+            this.lblGrades.AutoSize = true;
+            this.lblGrades.Location = new System.Drawing.Point(28, 77);
+            this.lblGrades.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblGrades.Name = "lblGrades";
+            this.lblGrades.Size = new System.Drawing.Size(41, 13);
+            this.lblGrades.TabIndex = 9;
+            this.lblGrades.Text = "Grades";
+            this.lblGrades.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtGrades
+            // 
+            this.txtGrades.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtGrades.Location = new System.Drawing.Point(73, 74);
+            this.txtGrades.Margin = new System.Windows.Forms.Padding(2);
+            this.txtGrades.Name = "txtGrades";
+            this.txtGrades.Size = new System.Drawing.Size(586, 20);
+            this.txtGrades.TabIndex = 8;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRun.AutoSize = true;
+            this.btnRun.Location = new System.Drawing.Point(674, 183);
+            this.btnRun.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 11;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // dlgOpenCsv
+            // 
+            this.dlgOpenCsv.Filter = "csv-File|*.csv";
+            // 
+            // gbxInput
+            // 
+            this.gbxInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbxInput.Controls.Add(this.btnApplications);
+            this.gbxInput.Controls.Add(this.txtApplications);
+            this.gbxInput.Controls.Add(this.lblGrades);
+            this.gbxInput.Controls.Add(this.btnGrades);
+            this.gbxInput.Controls.Add(this.lblSchedule);
+            this.gbxInput.Controls.Add(this.txtGrades);
+            this.gbxInput.Controls.Add(this.lblApplications);
+            this.gbxInput.Controls.Add(this.btnSchedule);
+            this.gbxInput.Controls.Add(this.txtSchedule);
+            this.gbxInput.Location = new System.Drawing.Point(11, 12);
+            this.gbxInput.Name = "gbxInput";
+            this.gbxInput.Size = new System.Drawing.Size(743, 104);
+            this.gbxInput.TabIndex = 12;
+            this.gbxInput.TabStop = false;
+            this.gbxInput.Text = "Input";
+            // 
+            // gbxOutput
+            // 
+            this.gbxOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbxOutput.Controls.Add(this.btnResults);
+            this.gbxOutput.Controls.Add(this.txtResults);
+            this.gbxOutput.Controls.Add(this.lblResults);
+            this.gbxOutput.Location = new System.Drawing.Point(11, 122);
+            this.gbxOutput.Name = "gbxOutput";
+            this.gbxOutput.Size = new System.Drawing.Size(743, 56);
+            this.gbxOutput.TabIndex = 13;
+            this.gbxOutput.TabStop = false;
+            this.gbxOutput.Text = "Output";
+            // 
+            // btnResults
+            // 
+            this.btnResults.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnResults.Location = new System.Drawing.Point(663, 18);
+            this.btnResults.Margin = new System.Windows.Forms.Padding(2);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(75, 23);
+            this.btnResults.TabIndex = 7;
+            this.btnResults.Text = "save";
+            this.btnResults.UseVisualStyleBackColor = true;
+            this.btnResults.Click += new System.EventHandler(this.btnResults_Click);
+            // 
+            // txtResults
+            // 
+            this.txtResults.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResults.Location = new System.Drawing.Point(73, 20);
+            this.txtResults.Margin = new System.Windows.Forms.Padding(2);
+            this.txtResults.Name = "txtResults";
+            this.txtResults.Size = new System.Drawing.Size(586, 20);
+            this.txtResults.TabIndex = 5;
+            // 
+            // lblResults
+            // 
+            this.lblResults.AutoSize = true;
+            this.lblResults.Location = new System.Drawing.Point(27, 23);
+            this.lblResults.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(42, 13);
+            this.lblResults.TabIndex = 6;
+            this.lblResults.Text = "Results";
+            this.lblResults.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // dlgSaveCsv
+            // 
+            this.dlgSaveCsv.Filter = "csv-File|*.csv";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(766, 647);
+            this.Controls.Add(this.gbxOutput);
+            this.Controls.Add(this.gbxInput);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtLog);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "CS TA Assignment";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.gbxInput.ResumeLayout(false);
+            this.gbxInput.PerformLayout();
+            this.gbxOutput.ResumeLayout(false);
+            this.gbxOutput.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.TextBox txtApplications;
+        private System.Windows.Forms.Label lblApplications;
+        private System.Windows.Forms.Button btnApplications;
+        private System.Windows.Forms.Button btnSchedule;
+        private System.Windows.Forms.Label lblSchedule;
+        private System.Windows.Forms.TextBox txtSchedule;
+        private System.Windows.Forms.Button btnGrades;
+        private System.Windows.Forms.Label lblGrades;
+        private System.Windows.Forms.TextBox txtGrades;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.OpenFileDialog dlgOpenCsv;
+        private System.Windows.Forms.GroupBox gbxInput;
+        private System.Windows.Forms.GroupBox gbxOutput;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.TextBox txtResults;
+        private System.Windows.Forms.Label lblResults;
+        private System.Windows.Forms.SaveFileDialog dlgSaveCsv;
+    }
+}
+
